5312b8c91a13c23ea4992a390fe5a5c86285d088
Reuben <reubendrummond@gmail.com>
2025-08-08 21:20:15 +1000

    fix pls

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import viteReact from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
